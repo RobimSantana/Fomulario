c5d619a2e408316493c3d997979ac53f22a1d41b
Robson Santana <robsonsantana.fusion@gmail.com>
2025-12-31 02:28:37 -0300

    Feat [certificado-form]: Criando a pagina de formularios

---
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { PrimaryButton } from '../../_components/primary-button/primary-button';
+
+@Component({
+  selector: 'app-certificado-form',
+  standalone: true,
+  imports: [ SecondaryButton, PrimaryButton ],
+  templateUrl: './certificado-form.html',
+  styleUrl: './certificado-form.css',
+})
+export class CertificadoForm {
+
+}
