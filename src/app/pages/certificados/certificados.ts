184eca45343ce0f5a2e1aa497a38e5c01478c841
Robson Santana <robsonsantana.fusion@gmail.com>
2025-12-31 01:37:23 -0300

    Feat [ page certificado]: gerando a page com certificado

---
+++ src/app/pages/certificados/certificados.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+//import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
+import { ItemCertificado } from '../../_components/item-certificado/item-certificado';
+
+@Component({
+  selector: 'app-certificados',
+  standalone: true,
+  imports: [ItemCertificado ],
+  templateUrl: './certificados.html',
+  styleUrl: './certificados.css',
+})
+export class Certificados {
+
+}
